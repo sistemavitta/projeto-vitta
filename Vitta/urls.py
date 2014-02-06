ed53ea20047353e63c40f310064be605d8ccd7f3
Bruno Alexandre <bruce_alex2006@hotmail.com>
2014-02-06 14:51:49 -0200

    @T22, Concluir configuracoes dos arquivos estaticos e a contrucao da url home renderizando o template base.html

--- Vitta/urls.py
+++ Vitta/urls.py
@@ -3,10 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from django.views.generic import TemplateView
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Vitta.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    #class based view para renderizer a home do sistema
+    url(r'^$', TemplateView.as_view(template_name='core/base.html'), name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+
+
+
+urlpatterns = patterns('core.views',
+
+    #url(r'^$', TemplateView.as_view(template_name='core/base.html'), name='home'),
+
+)
