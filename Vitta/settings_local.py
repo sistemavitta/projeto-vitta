b46c89e8587c8e54ff77f42c804f1755807a3cdc
Bruno Alexandre <bruce_alex2006@hotmail.com>
2014-02-21 13:34:23 -0300

    heroku-banco local

--- Vitta/settings_local.py
+++ Vitta/settings_local.py
@@ -1,2 +1,17 @@
 
-teste
+'''
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'vitta.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not sqlite 3
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+'''
