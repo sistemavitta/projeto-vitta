f1e627eb3585bd3e2d0ed94045877f81a2babbad
Bruno Alexandre <bruce_alex2006@hotmail.com>
2014-02-21 17:02:01 -0300

    @T55, criar perfil dos usuarios do sistema

--- perfil/admin.py
+++ perfil/admin.py
@@ -1,3 +1,23 @@
+#encoding: utf-8
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from perfil.models import Perfil
+from filebrowser.widgets import FileInput
 
 # Register your models here.
+
+class PerfilInline(admin.StackedInline):
+    model = Perfil
+    can_delete = False
+    inline_classes = ('grp-collapse grp-open',)
+    verbose_name_plural = u'Informações'
+
+
+class UserAdmin(UserAdmin):
+	
+    inlines = (PerfilInline, )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,35 @@
+#encoding: utf-8
 from django.db import models
+from django.contrib.auth.models import User
+from filebrowser.fields import FileBrowseField
 
 # Create your models here.
+class Perfil(models.Model):
+
+    LEVEL_CHOICES=(
+                  (1,u"Administrador"),
+                  (2,u"Professor"),
+                  (3,u"Aluno"),                                
+                  )
+    image = FileBrowseField(u"Imagem",
+    						max_length=200,
+    						directory="images/",
+    						extensions=[".jpg",".png"],
+    						blank=True, null=True)
+    phone = models.CharField(u'Telefone', max_length=13)
+    user = models.OneToOneField(User,
+								verbose_name=u'Usuário',
+								related_name='perfil')
+    level = models.IntegerField(choices=LEVEL_CHOICES,
+    						verbose_name=u'Nível')
+
+
+
+    class Meta:
+    	verbose_name=u'Perfil'
+    	verbose_name_plural=u'Perfis'
+
+
+    def __unicode__(self):
+        return (self.user)
+    
