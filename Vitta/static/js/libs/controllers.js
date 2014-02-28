5904181b4e20a647d3b311c8e24ec5e95f263136
Bruno Alexandre <bruce_alex2006@hotmail.com>
2014-02-28 19:17:48 -0300

    heroku atualização

--- Vitta/static/js/libs/controllers.js
+++ Vitta/static/js/libs/controllers.js
@@ -1,17 +1,25 @@
 var mod = angular.module('home', [])
 
-function BuscarAluno($scope){
+ 
+mod.config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('[[');
+  $interpolateProvider.endSymbol(']]');
+});
 
 
 
-  $scope.buscar = function(){
-        var path = 'http://vitta.herokuapp.com/talks/users/';
+mod.controller('BuscarAluno', ['$scope', function($scope,$http){
+
+	$scope.buscar = function(){
+        var path = '/talks/users/';
         $scope.loading = true;
         $.get(path).success(function(data){
             $scope.loading = false;
             $scope.usuarios = data.results;
-            $scope.$digest()
-        });
+            $scope.$digest();
+        })
     };
+	
 
-}
+
+}]);
