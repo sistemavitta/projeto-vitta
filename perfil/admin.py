4acf835a2b6fa0d6790e3fa5de00dfd2dc514153
brucealex <bruce_alex2006@hotmail.com>
2014-03-30 11:17:40 -0300

    @T147, heroku conf 2

--- perfil/admin.py
+++ perfil/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from perfil.models import Perfil
-from filebrowser.widgets import FileInput
+
 
 # Register your models here.
 
