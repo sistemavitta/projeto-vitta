809af0176c56ccfbb4a09ea5052196b1f4a653ed
brucealex <bruce_alex2006@hotmail.com>
2014-03-31 13:12:06 -0300

    @T164, criar tabela Presenças no banco de dados

--- administration/admin.py
+++ administration/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from models import AdministrationTemp
+from models import AdministrationTemp, Presenca
 
 class AdministrationTempAdmin(admin.ModelAdmin):
     list_display=['professor','aluno','imagem','ficha','treino','treinando']
     #search_fields=['aluno__nome']
 
+
+class PresencaAdmin(admin.ModelAdmin):
+    list_display=['data_inicio','aluno','treino','professor']
+
+
 admin.site.register(AdministrationTemp,AdministrationTempAdmin)
+admin.site.register(Presenca,PresencaAdmin)
--- administration/models.py
+++ administration/models.py
@@ -37,3 +37,43 @@ class AdministrationTemp(models.Model):
         ordering = ['aberta_em']
 
 
+class Presenca(models.Model):
+
+    aluno=models.ForeignKey(User,
+                            verbose_name=u'Aluno',
+                            related_name=u'presencas')
+    treino=models.ForeignKey(Treinos,
+                            verbose_name=u'Treino',
+                            related_name=u'treino')
+    professor = models.ForeignKey(User,
+                                 verbose_name=u'Professor',
+                                 related_name='treinamentos')
+    data_inicio=models.DateTimeField(verbose_name=u'Data Inicio',
+                              null=True, blank=True)
+    data_fim=models.DateTimeField(verbose_name=u'Data Fim',
+                                  null=True, blank=True)
+    feedback=models.TextField(verbose_name=u"Feedback do Treino",
+                               blank=True, null=True)
+
+    # def horas(self,dias = 0, horas = 0, minutos = 0, segundos = 0, microssegundos= 0, intervalo = None, tempo = None):
+    #     total_horas = dias * 24 + horas + minutos / 60.00 + segundos / 3600.00 + microssegundos / 3600000000.00
+    #     if intervalo:
+    #         total_horas += intervalo.days * 24 + intervalo.seconds / 3600.00 + intervalo.microseconds / 3600000000.00
+    #     if tempo:
+    #         total_horas += tempo.hour + tempo.minute / 60.00 + tempo.second /3600.00 + tempo.microsecond / 3600000000.00
+    #     return total_horas
+
+    # def duracao(self):
+    #     intervalo=self.data_fim - self.data
+    #     tempo= (intervalo.days * 24 + intervalo.seconds / 3600.00 + intervalo.microseconds / 3600000000.00 )
+    #     tempo=" %s min" % int(tempo*60)
+    #     return tempo
+    # duracao.short_description=u'Duração'
+
+    def __unicode__(self):
+        return unicode(self.data_inicio)
+
+    class Meta:
+      verbose_name_plural=u'Presenca dos Alunos'
+      ordering=['data_inicio']
+
