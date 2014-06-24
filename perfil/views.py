5d5a61bfc620f4541e2a0cbe8834ae2657c37848
brucealex <bruce_alex2006@hotmail.com>
2014-06-24 16:15:28 -0300

    @T219 - alteração na mensagem de alerta finalizar treinamento

--- perfil/views.py
+++ perfil/views.py
@@ -75,7 +75,7 @@ class FecharFichaView(LoginRequiredMixin,ContextalunoMixim,View,ContextMixin):
             return HttpResponseRedirect(reverse('perfil-detail', kwargs={'pk': request.user.pk}))
 
         context = self.get_context_data(**kwargs)
-        context['alerta']='Aluno Treinando: Finalize o treinamento para poder fechar a ficha'
+        context['alerta']='Finalize o treinamento para poder fechar a ficha.'
         return render (request,self.template_name, context)
 
 
